6702912c1aeb28f321d158a3c320614337ae354f
craigh210 <craighunt210@gmail.com>
2019-03-26 12:29:21 -0400

    303 first update

--- src/main/java/com/cwhcode/springboot303/Course.java
+++ src/main/java/com/cwhcode/springboot303/Course.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -27,7 +28,7 @@ public class Course {
     private String description;
 
     @NotNull
-    @Size(min=3)
+    @Min(3)
     private int credit;
 
     public long getId() {
